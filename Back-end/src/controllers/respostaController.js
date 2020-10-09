4714ebc16fe8ded75f09a14841d6fd18d9ad15a2
danilo-uea <dsf.eng17@uea.edu.br>
2020-10-09 14:03:06 -0400

    gitignore

--- Back-end/src/controllers/respostaController.js
+++ Back-end/src/controllers/respostaController.js
@@ -6,7 +6,6 @@ module.exports = {
 
     async PerguntaResposta(req, res){
         const {id} = req.params;
-        console.log(id);
         //const {page = 1} = req.query;
         const respostas = await Resposta.find({"pergunta": id}).populate('usuario');
         //const respostas = await Resposta.paginate({"idPergunta": id}, {page, limit: 10});
